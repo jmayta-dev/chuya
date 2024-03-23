1f716f5c293f93748f80b346f731979bcd0643b0
Jheison J. Mayta C <8401842+jmayta-dev@users.noreply.github.com>
2024-03-23 06:42:55 -0500

    🚧 add BaseEntity abstract class

---
+++ src/MW.CHUYA.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using MW.CHUYA.Domain.Common.Interfaces;
+
+namespace MW.CHUYA.Domain.Common;
+
+public abstract class BaseEntity<TId> : IEntity<TId>
+{
+    // private
+    // public
+    public TId? Id { get; set; }
+}
