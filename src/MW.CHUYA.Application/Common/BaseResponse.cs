0a11e5a094862867e415161db11dfa1b13ff593b
Jheison J. Mayta C <8401842+jmayta-dev@users.noreply.github.com>
2024-04-11 23:53:25 -0500

    🚧 add base response common class

---
+++ src/MW.CHUYA.Application/Common/BaseResponse.cs
@@ -0,0 +1,9 @@
+namespace MW.CHUYA.Application.UseCases.Common.Base;
+
+public class BaseResponse<T>
+{
+    public bool IsSuccess { get; set; }
+    public T? Data { get; set; }
+    public string? Message { get; set; }
+    public IEnumerable<BaseError>? Errors { get; set; }
+}
