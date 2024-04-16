1643295838055de4861b382d9c2341ac64a98fcd
Jheison J. Mayta C <8401842+jmayta-dev@users.noreply.github.com>
2024-04-15 21:12:49 -0500

    add 'updated by' property to IAuditableEntity

--- src/MW.CHUYA.Domain/Common/Interfaces/IAuditableEntity.cs
+++ src/MW.CHUYA.Domain/Common/Interfaces/IAuditableEntity.cs
@@ -5,6 +5,7 @@ public interface IAuditableEntity<TId> : IAuditableEntity, IEntity<TId>
 {
     int CreatedBy { get; set; }
     DateTime CreatedAt { get; set; }
+    int UpdatedBy { get; set; }
     DateTime UpdatedAt { get; set; }
 }
 
